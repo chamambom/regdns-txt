61c5e3b4f3c5ace8dbda5db9e9e8a9e745d47dc3
root <chamambom@afri-com>
2015-01-25 12:05:40 +0200

    dealing with complex queries phase 1

--- domaindetail.php
+++ domaindetail.php
@@ -8,7 +8,13 @@ $domain_id=$_GET['domain_id'];
 include('dbconnector.php');
 
 
-$selectdetail="SELECT * FROM domain_details WHERE domain_id=$domain_id";
+//$selectdetail="SELECT * FROM domain_details WHERE domain_id=$domain_id";
+
+$selectdetail ="SELECT domain_details.*, address.*
+FROM domain_details
+INNER JOIN address
+ON domain_details.domain_id=address.domain_id WHERE domain_details.domain_id=$domain_id";
+
 
 $result=mysqli_query($link,$selectdetail);
 								 
@@ -17,105 +23,105 @@ if(mysqli_num_rows($result)>0)
 	$row=mysqli_fetch_assoc($result);
 	
 	   echo"<h4> Below are the details you entered for domain ". $row['domain_name'] ." with domain Owner ". $row['domainOwner'] ." </h4>";								 
-	   echo"<div id='full_table_template'>";
+	   echo"<div id='full_template'>";
+	   
 	   echo "1.  DOMAIN NAME and ACTION";	
 	
 	   echo"<div class='form-row'>
-		<div><label for='Gender'>* 1a. Full domain name......................:</label><span class='field-widget'> ". $row['domain_name'] ." </span></div>";
+		<div><label for='DomainName'>* 1a. Full domain name......................:</label><span class='field-widget'> ". $row['domain_name'] ." </span></div>";
 		
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Title'>* 1b. (N)ew or (M)odify or (D)elete.(N/M/D).:</label><span class='field-widget2'> ". $row['domstatus'] ." </span></div>";
+		<div class='field-label'><label for='DomainStatus'>* 1b. (N)ew or (M)odify or (D)elete.(N/M/D).:</label><span class='field-widget2'> ". $row['domstatus'] ." </span></div>";
 
        echo "2.  DOMAIN OWNER";
 
 	   echo"<div class='form-row'>
-		<div ><label for='Nationality'>* 2a. Domain Owner..........:</label><span class='field-widget'> ". $row['domainOwner'] ." </span></div>";
+		<div ><label for='DomainOwner'>* 2a. Domain Owner..........:</label><span class='field-widget'> ". $row['domainOwner'] ." </span></div>";
 
 
 	   echo"<div class='form-row'>
-		<div class='field-label' ><label for='Position'>* 2b. Organisation Name.....:</label><span class='field-widget2'> ". $row['domain_owner_org_name'] ." </span></div>";
+		<div class='field-label' ><label for='OrganisationNmae'>* 2b. Organisation Name.....:</label><span class='field-widget2'> ". $row['domain_owner_org_name'] ." </span></div>";
 
 
 	   echo"<div class='form-row'>
-		<div ><label for='Telephone Number'>* 2c. Physical Address......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
+		<div class='field-label' ><label for='PhysicalAddress'>* 2c. Physical Address......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
 			  
 	   echo"<div class='form-row'>
-		<div ><label for='Telephone Number'>* 2d. Postal Address .......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
+		<div class='field-label'><label for='PostalAddress'>* 2d. Postal Address .......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
 
 
 	
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Alternative Telephone'>* 2e. Town/City.............:</label><span class='field-widget2'> ". $row['Telephone2'] ." </span></div>";
+		<div class='field-label'><label for='TownCity'>* 2e. Town/City.............:</label><span class='field-widget2'> ". $row['city'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 2f. Country...............:</label><span class='field-widget'> ". $row['name'] ." </span></div>";
+		<div class='field-label'><label for='Country'>* 2f. Country...............:</label><span class='field-widget'> ". $row['country_id'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Alternarnative '>* 2g. Voice Phone...........:</label><span class='field-widget2'> ". $row['EmailAddress2'] ." </span></div>";
+		<div class='field-label'><label for='Mobile_Work_Phone '>* 2g. Voice Phone...........:</label><span class='field-widget2'> ". $row['EmailAddress2'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Country Based'>2h. Fax Number............:</label><span class='field-widget'> ". $row['CountryBased'] ." </span></div>";
+		<div class='field-label'><label for='FaxNumber'>2h. Fax Number............:</label><span class='field-widget'> ". $row['CountryBased'] ." </span></div>";
 		
 			   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 2i. E-mail Address........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='EmailAddress'>* 2i. E-mail Address........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 
 echo "3.  ADMIN/BILLING CONTACT";
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3a. ZISPA Handle..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='BillingZispaHandle'>* 3a. ZISPA Handle..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3b. Contact Name..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='BillingContactName'>* 3b. Contact Name..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3d. Physical Address .....:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
+		<div class='field-label'><label for='BillingPhysicalAddress'>* 3d. Physical Address .....:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3e. Postal Address .......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
+		<div class='field-label'><label for='BillingPostalAddress'>* 3e. Postal Address .......:</label><span class='field-widget'> ". $row['address_detail'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3f. Town/City.............:</label>:</div>
-		<span class='field-widget'> ". $row['EmailAddress1'] ." </span>";
+		<div class='field-label'><label for='BillingTownCity'>* 3f. Town/City.............:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div ><label for='Email Address'>* 3g. Country...............:</label><span class='field-widget'> ". $row['name'] ." </span></div>";
+		<div class='field-label'><label for='BillingCountry'>* 3g. Country...............:</label><span class='field-widget'> ". $row['name'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 3h. Voice Phone...........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='BillingMobileWorkPhone'>* 3h. Voice Phone...........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	 echo"<div class='form-row'>
-	<div class='field-label'><label for='Email Address'>3i. Fax Number............:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+	<div class='field-label'><label for='BillingFaxNumber'>3i. Fax Number............:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	 echo"<div class='form-row'>
-	<div class='field-label'><label for='Email Address'>* 3j. E-mail Address........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+	<div class='field-label'><label for='BillingEmailAddress'>* 3j. E-mail Address........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 		
 		echo "4.DESCRIPTION OF ORGANISATION/DOMAIN";
 		
 	echo"<div class='form-row'>
-	<div class='field-label'><label for='Email Address'>* 4a. Description of domain owner's organisation..:</label><span class='field-widget'> ". $row['domain_owner_org_desc'] ." </span></div>";
+	<div class='field-label'><label for='OrganisationDescription'>* 4a. Description of domain owner's organisation..:</label><span class='field-widget'> ". $row['domain_owner_org_desc'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 4b. Proposed domain usage.:</label><span class='field-widget'> ". $row['domain_usage'] ." </span></div>";
+		<div class='field-label'><label for='DomainUsage'>* 4b. Proposed domain usage.:</label><span class='field-widget'> ". $row['domain_usage'] ." </span></div>";
 		
       echo "5.  TECHNICAL CONTACT";
 	  
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 5a. ZISPA Handle..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='TechZispaHandle'>* 5a. ZISPA Handle..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 5b. Contact Name..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='TechContactName'>* 5b. Contact Name..........:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div ><label for='Email Address'>* 5c. Organisation Name.....:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='TechOrganisationName'>* 5c. Organisation Name.....:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 5d. Physical Address .....:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='TechPhysicalAddress'>* 5d. Physical Address .....:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 5e. Postal Address .......:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
+		<div class='field-label'><label for='TechPostalAddress'>* 5e. Postal Address .......:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
 
 	   echo"<div class='form-row'>
-		<div class='field-label'><label for='Email Address'>* 5f. Town/City.............:</label><span class='field-widget'> ". $row['name'] ." </span></div>";
+		<div class='field-label'><label for='TechTownCity'>* 5f. Town/City.............:</label><span class='field-widget'> ". $row['name'] ." </span></div>";
 
 	   echo"<div class='form-row'>
 		<div class='field-label'><label for='Email Address'>* 5g. Country...............:</label><span class='field-widget'> ". $row['EmailAddress1'] ." </span></div>";
