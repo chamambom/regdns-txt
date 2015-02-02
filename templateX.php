6280c4d972a1fd6efd326b5d4275c0334bbaebcf
root <chamambom@afri-com>
2015-02-02 12:06:19 +0200

    working on the mail uploader

--- templateX.php
+++ templateX.php
@@ -1,10 +1,12 @@
+<!DOCTYPE html>
 <html>
-  <head>
-  	<title></title>
+<head>
+    <meta charset='utf-8'>
+
+<link rel="stylesheet" href="css/mail.css"/>
 <link rel="stylesheet" type="text/css" href="css/domtables.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
-<body>
 <?php
 session_start();
 
@@ -149,14 +151,128 @@ $downloadFileName = ZwDomainTemplate($full_domain_name, $domain_status, $domain_
 
 echo"<h4> Please attach associated documents for domain $full_domain_name with domain Owner $domain_owner before sending to ZISPA </h4>";	
 
- 
- 
+
+
+?>
+
+
+<body>
+
+<?php 
 
 echo"<div class='full_template'>";
 echo"You are about to send template " . $downloadFileName . " to Zispa" . "<a href='./{$downloadFileName}'>View Template</a>";
 ?>
 
-<p> attache</p>
+<div class="title">Attach Supportin Docs Here</div>
+	<div class="mail">
+	  <form action='mail.php' method='post' id='mailForm' enctype='multipart/form-data'>
+	    <table border="0">
+		     <tr>
+			    <td class="label"> Name : </td>
+				<td><input type="text" id="name" name="name" class="form-input" placeholder='User Name'/>
+				  <div id="invalid-name" class="error_msg"></div>
+				</td>
+			 </tr>
+			 <tr>
+			    <td class="label"> E-mail : </td>
+				<td><input type="email" id="email" name="email" class="form-input" placeholder='E-Mail'/>
+				   <div id="invalid-email" class="error_msg"></div>
+				</td>
+			 </tr>
+			 <tr>
+			    <td class="label"> From E-mail : </td>
+				<td><input type="email" id="femail" name="femail" class="form-input" placeholder='From E-Mail'/>
+				   <div id="invalid-femail" class="error_msg"></div>
+				</td>
+			 </tr>
+			 
+			 <tr>
+			    <td class="label"> Phone : </td>
+				<td><input type="tel" id="phone" name="phone" class="form-input" placeholder='Phone'/>
+				   <div id="invalid-phone" class="error_msg"></div>
+				</td>
+			 </tr>
+			 <tr>
+			    <td class="label"> Image : </td>
+				<td><input type="file" id="image" name="image" class="form-input" placeholder='Image'> <div id="invalid-image" class="error_msg"></div></td>
+				
+			 </tr>
+			 
+			 <tr>
+			    <td class="label"> Message : </td>
+				<td><textarea name="message" placeholder='Message' ></textarea>
+
+                
+                <span id="invalid-message" class="error_msg"></span></td>
+				
+			 </tr>
+			 
+			 <tr>
+			    <td colspan="2"> <input type="submit" value="Send Mail!" id='submit_btn' name="submit_btn" class="submit_btn"/></td>
+			 </tr>
+		</table>
+		</form>
+        </div>
+       	<script src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery.validate.min.js"></script>
+	<script>
+	  (function($){
+	     jQuery.validator.setDefaults({
+			errorPlacement: function(error, element) {
+				error.appendTo('#invalid-' + element.attr('id'));
+			}
+		});
+	     $("#mailForm").validate({
+		        rules: {
+					name: {
+					   required: true,
+					   minlength : 3,
+					},
+					email:{ 
+					   required: true,
+					   email: true,
+					},
+					phone: { 
+					  required : true,
+					  number:true,	
+					  minlength : 10,
+				      maxlength : 11
+					},
+					image: "required",
+					message: "required",
+					femail:{ 
+					   required: true,
+					   email: true,
+					}
+				},
+				messages: {
+					name: {
+					   required:"Please enter your name",
+					   minlength: "Please enter a valid name",
+					},
+					email:{ 
+					   required: "Please enter your email",
+					   minlength: "Please enter a valid email address",
+					},
+					phone: { 
+					   required: "Please enter your phone number",
+					   minlength: "Please enter your valid phone number",
+					   maxlength: "Please enter your valid phone number"
+					},
+					image: "Please Choose your image",
+					message: "Please enter your message",
+					femail: { 
+					   required: "Please enter your email",
+					   minlength: "Please enter a valid email address",
+					}
+				}
+		 });
+	  })($);
+	</script>
+
+     
+
 
 
 <?php
