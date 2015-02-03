9dddcfc85728c4ccd4079fa8b3d0f2ab57964e32
root <chamambom@afri-com>
2015-02-03 19:19:13 +0200

    file attach 01

--- mail.php
+++ mail.php
@@ -71,7 +71,7 @@ try {
 	//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
 	//$mail->WordWrap   = 0; // set word wrap	
 	$mail->AddAttachment($_FILES['image']['tmp_name'], $_FILES['image']['name']);
-	$mail->AddAttachment($_FILES['templatex']['tmp_name_1'], $_FILES['templatex']['name_1']);
+	$mail->AddAttachment($_FILES['templatex']['tmp_name'], $_FILES['templatex']['name']);
 
     $mail->AddAttachment($downloadFileName,$downloadFileName);	
 	
