5f713e6f94222fecb5528f4053751d3726621c61
root <chamambom@afri-com>
2015-02-03 06:54:02 +0200

    mail attach 20

--- mail.php
+++ mail.php
@@ -70,7 +70,8 @@ try {
 	//$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
 	//$mail->WordWrap   = 80; // set word wrap	
 	$mail->AddAttachment($_FILES['image']['tmp_name'], $_FILES['image']['name']);
-    $mail->AddAttachment($downloadFileName);	
+	$encoding = 'base64';
+    $mail->AddAttachment($downloadFileName,$encoding);	
 	
 	$mail->IsHTML(false); // send as HTML
 	$mail->Send();
