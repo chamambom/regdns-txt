8be7d73298b92927c6ea16072bae208c4dde30b3
root <chamambom@afri-com>
2015-02-19 12:49:03 +0200

    Added another page mailsuccess.php for email redirection to avoid template being sent twice

--- mail.php
+++ mail.php
@@ -1,9 +1,5 @@
 <?php session_start(); ?>
 
-<link rel="stylesheet" href="css/mail.css"/>
-<link rel="stylesheet" type="text/css" href="css/domtables.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-
 <?php
 /**
 * Simple example script using PHPMailer with exceptions enabled
@@ -78,19 +74,14 @@ try {
 	
 	
 	
-	echo"<div class='header'> Thank you very much $registrant for sending $full_domain_name template to ZISPA, Check your email  " .$mail->From  ." for ZISPA Confirmation  </div>";	
-		
-	echo"<div class='full_template'>";
-	echo 'Your Template has been sent to ZISPA.';
+	
 	
 
 } catch (phpmailerException $e) {
 	echo $e->errorMessage();
 }
-echo "</div>";
 
-echo "<div class='footer'>Courtesy of the Service Management Center (SMC) Africom @2015 <a href='http://www.afri-com.net'>www.afri-com.net</a></div>";
 
-//header('Location: templateX.php');
+header('Location: mailsuccess.php');
 
 ?>
---
+++ mailsuccess.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<link rel="stylesheet" href="css/mail.css"/>
+<link rel="stylesheet" type="text/css" href="css/domtables.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+<?php
+
+$full_domain_name =$_SESSION['domain_name'];
+
+$downloadFileName=$_SESSION['downloadFileName'];
+
+$registrant= $_SESSION['name']; 
+
+
+echo"<div class='header'> Thank you very much $registrant for sending $full_domain_name template to ZISPA, Check your email  dns-admin@africominternet.co.zw for ZISPA Confirmation  </div>";	
+		
+	echo"<div class='full_template'>";
+	echo 'Your Template has been sent to ZISPA.';
+	
+echo "</div>";
+
+
+echo "<div class='footer'>Courtesy of the Service Management Center (SMC) Africom @2015 <a href='http://www.afri-com.net'>www.afri-com.net</a></div>";
+
+
+?>
